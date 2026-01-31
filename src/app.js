ceae55ba177b881fdf5805b5265155dcbd3433f9
María Almendras <almendrasmaria04@gmail.com>
2026-01-30 21:58:43 -0300

    config package.json e inicio basico app express

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola!');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+}); 
